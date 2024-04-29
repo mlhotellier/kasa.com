547fe5826b0de7245ef051f4e219e0caae390ba1
Mathis L'Hôtellier <mathislhotellier@gmail.com>
2024-04-29 12:16:24 +0200

    Add logo, title and favicon. Creating the project structure

---
+++ src/pages/Home/index.js
@@ -0,0 +1,16 @@
+import logo from '../../assets/logo-kasa.svg'
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <p>
+          Bienvenue sur <code>Kasa.com</code>
+        </p>
+        <img src={logo} alt="logo Kasa" />
+      </header>
+    </div>
+  )
+}
+
+export default App
